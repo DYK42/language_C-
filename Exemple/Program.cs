89e6378ac5553b35b950c49bad4d1821a6094ef1
DYK42 <yurandk42@gmail.com>
2022-08-04 10:39:11 +0700

    Add incoming data

--- Exemple/Program.cs
+++ Exemple/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите скорость первого друга!");
+double speedFriend1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите скорость второго друга! Должна быть меньше скорости первого друга!");
+double speedFriend2 = Convert.ToDouble(Console.ReadLine());
+
+if(speedFriend1<speedFriend2) {
+    Console.WriteLine("Неверный параметр. Попробуйте еще раз!");
+    return;
+}
+
+Console.WriteLine("Введите скорость собаки! Должна быть больше скоростей обоих друзей!");
+double speedDog = Convert.ToDouble(Console.ReadLine());
+
+if(speedDog<speedFriend1 && speedDog<speedFriend2) {
+    Console.WriteLine("Неверный параметр. Попробуйте еще раз!");
+    return;
+}
