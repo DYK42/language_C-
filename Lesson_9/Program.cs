db49ca0d184220c64b11e561ab80260199dda34c
DYK42 <yurandk42@gmail.com>
2022-08-31 16:16:01 +0700

    add Task 59 Sem 9

--- Lesson_9/Program.cs
+++ Lesson_9/Program.cs
@@ -44,7 +44,7 @@ int[,] array = GetArray(5, 6, 0, 50);
 PrintArray(array);
 PrintArray(ChangeRows(array));
 */
-
+/*
 int[] GetSingleArray(int[,] array)
 {
     int[] result = new int[array.GetLength(0) * array.GetLength(1)];
@@ -108,3 +108,69 @@ Console.WriteLine();
 Console.Write($"{string.Join(", ", singleArray)}");
 Console.WriteLine();
 GetCountNumber(singleArray);
+*/
+
+/*Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, 
+на пересечении которых расположен наименьший элемент массива.*/
+
+int[] FindIndexOfMinArray(int[,] arr)
+{
+    int[] IndexOfMinArr = new int[2];
+    int RowMin = 0;
+    int ColMin = 0;
+    int minArr = arr[0, 0];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] < minArr)
+            {
+                minArr = arr[i, j];
+                RowMin = i;
+                ColMin = j;
+            }
+        }
+        IndexOfMinArr[0] = RowMin;
+        IndexOfMinArr[1] = ColMin;
+    }
+    return IndexOfMinArr;
+}
+
+int[,] CorrectArrayToMinimum(int[,] arr)
+{
+    int[,] CorrectArr = new int[arr.GetLength(0) - 1, arr.GetLength(1) - 1];
+    int correctI = 0;
+    int correctJ = 0;
+
+    for (int i = 0; i < CorrectArr.GetLength(0); i++)
+    {
+        if (i >= FindIndexOfMinArray(arr)[0]) correctI= i+1;
+        else correctI = i;
+
+        for (int j = 0; j < CorrectArr.GetLength(1); j++)
+        {
+            if (j >= FindIndexOfMinArray(arr)[1]) correctJ = j+1;
+            else correctJ = j;
+            
+            CorrectArr[i, j] = arr[correctI, correctJ];
+        }
+    }
+    return CorrectArr;
+}
+
+Console.Write("Число строк: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Число столбцов: ");
+int col = int.Parse(Console.ReadLine());
+Console.Write("Начало диапазона значений: ");
+int beginNum = int.Parse(Console.ReadLine());
+Console.Write("Окончание диапазона: ");
+int endNum = int.Parse(Console.ReadLine());
+
+int[,] myArray = GetArray(row, col, beginNum, endNum);
+PrintArray(myArray);
+Console.WriteLine();
+Console.WriteLine($"Наименьший элемент массива расположен на пересечении {String.Join(", ", FindIndexOfMinArray(myArray))}");
+myArray = CorrectArrayToMinimum(myArray);
+Console.WriteLine();
+PrintArray(myArray);
